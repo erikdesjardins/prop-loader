998e3d995d0b38201ac85866c22187a2d40f51b0
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2016-03-04 12:38:08 -0500

    testing: remove runLoader, use loader.call directly

--- test/test.js
+++ test/test.js
@@ -9,13 +9,6 @@ const json = JSON.stringify({
 	nil: null
 });
 
-function runLoader(data, query) {
-	const context = {
-		query: typeof query === 'undefined' ? '' : `?${query}`
-	};
-	return loader.call(context, data);
-}
-
 test('calls cacheable', t => {
 	t.plan(1);
 	const context = {
@@ -25,46 +18,46 @@ test('calls cacheable', t => {
 });
 
 test('no query', t => {
-	t.is(loader(json), 'module.exports = {"version":"1.2.3","background":{"scripts":["background.js"]},"nil":null};');
+	t.is(loader.call({}, json), 'module.exports = {"version":"1.2.3","background":{"scripts":["background.js"]},"nil":null};');
 });
 
 test('single property query', t => {
-	t.is(runLoader(json, 'version'), 'module.exports = "1.2.3";');
+	t.is(loader.call({ query: '?version' }, json), 'module.exports = "1.2.3";');
 });
 
 test('deep query', t => {
-	t.is(runLoader(json, 'background.scripts'), 'module.exports = ["background.js"];');
+	t.is(loader.call({ query: '?background.scripts' }, json), 'module.exports = ["background.js"];');
 });
 
 test('null-valued property', t => {
-	t.is(runLoader(json, 'nil'), 'module.exports = null;');
+	t.is(loader.call({ query: '?nil' }, json), 'module.exports = null;');
 });
 
 test('fails on invalid json', t => {
-	t.throws(() => loader('{'), 'Unexpected end of input');
+	t.throws(() => loader.call({}, '{'), 'Unexpected end of input');
 });
 
 test('fails on empty query', t => {
-	t.throws(() => runLoader(json, ''), 'Property "" not found');
+	t.throws(() => loader.call({ query: '?' }, json), 'Property "" not found');
 });
 
 test('fails on non-existent property', t => {
-	t.throws(() => runLoader(json, 'notARealProperty'), 'Property "notARealProperty" not found');
+	t.throws(() => loader.call({ query: '?notARealProperty' }, json), 'Property "notARealProperty" not found');
 });
 
 test('fails on non-existent deep property', t => {
-	t.throws(() => runLoader(json, 'background.foobar'), 'Property "background.foobar" not found');
+	t.throws(() => loader.call({ query: '?background.foobar' }, json), 'Property "background.foobar" not found');
 });
 
 test('passing a js object', t => {
-	const result = runLoader({ a: { b: { c: { d: ['e'] } } } }, 'a.b.c');
+	const result = loader.call({ query: '?a.b.c' }, { a: { b: { c: { d: ['e'] } } } });
 	t.is(result, 'module.exports = {"d":["e"]};');
 });
 
 test('null, no query', t => {
-	t.is(runLoader(null), 'module.exports = null;');
+	t.is(loader.call({}, null), 'module.exports = null;');
 });
 
 test('fails on null with query', t => {
-	t.throws(() => runLoader(null, 'prop'), "Cannot use 'in' operator to search for 'prop' in null");
+	t.throws(() => loader.call({ query: '?prop' }, null), "Cannot use 'in' operator to search for 'prop' in null");
 });
